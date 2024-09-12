3b13dda5ae80884edb796503cfbb41376790b346
mindaaaa <devalc735@gmail.com>
2024-09-12 09:34:58 +0900

    fix: calculate 함수  무한루프 문제 해결

--- src/domain/calculate.js
+++ src/domain/calculate.js
@@ -9,43 +9,46 @@ const priority = [
   },
 ];
 
+// input:
+// expr: [123,'+',24,'*',32], oper: {
+//   '*': (a, b) => a * b,
+//   '/': (a, b) => a / b,
+// }
+// output:
+// 768
 export function calOper(expr, oper) {
   const result = [];
   let operator = null;
   for (const token of expr) {
     if (token in oper) {
-      operator = oper[token];
+      operator = oper[token]; // 연산자면 가져오고
     } else if (operator) {
+      // 두번째 연산자면 계산
       result[result.length - 1] = operator(result[result.length - 1], token);
       operator = null;
     } else {
-      result.push(token);
+      result.push(token); // 첫 번째 연산자면 result 배열에 삽입
     }
   }
   return result;
-  // input:
-  // expr: [24,*,32], oper: *
-  // output:
-  // 768
 }
 
-// token 배열에 영향 ... 원본 배열 바꿔가면서.
-// 원본 배열에 영향을 주는 함수
-
-// 4. 연산자를 처리하는 함수를 만든다.
+// input: [123,'+',24,'*',32]
+// 연산자 우선순위를 정리해
+// 1. [24,'*',32]
+// 2. [123,'+',768]
+// output: 891
 export function calculate(tokens) {
+  // [123,'+',24,'*',32]
   for (let i = 0; i < priority.length; i++) {
+    // 우선순위 2회 들어감
     while (tokens.length > 1) {
+      // tokens가 1일 때까지 동작
       const newToken = calOper(tokens, priority[i]);
 
       tokens = newToken;
+      if (tokens.length === newToken.length) break; // 우선순위 연산 끝난 후 로직 나오기
     }
   }
-
   return tokens[0];
-  // input: [123,+,24,*,32]
-  // 연산자 우선순위를 정리해
-  // 1. [24,*,32]
-  // 2. [123,+,768]
-  // output: 891
 }
--- test/domain/calculate.test.js
+++ test/domain/calculate.test.js
@@ -1,5 +1,22 @@
 import { calOper, calculate } from '../../src/domain/calculate';
 
+describe.skip('calculate.js', () => {
+  test('test', () => {
+    // given
+    const expr = [24, '*', 32];
+    const oper = {
+      '*': (a, b) => a * b,
+      '/': (a, b) => a / b,
+    };
+
+    // when
+    const result = calOper(expr, oper);
+
+    // then
+    expect(result).toEqual([768]);
+  });
+});
+
 describe('calculate.js', () => {
   test('test', () => {
     // given
